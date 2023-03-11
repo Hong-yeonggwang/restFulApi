67d643c97018137dd1c396e2bb0fbf43911e166c
Hong-yeonggwang <chamsk0808@naver.com>
2023-03-11 22:46:14 +0900

    엔티티 컬럼 생성시 _ 사용 불가. jparepository 메서드 생성시 오류 존재

--- src/main/java/com/example/restfulAPI/JPA/Member.java
+++ src/main/java/com/example/restfulAPI/JPA/Member.java
@@ -12,14 +12,17 @@ import javax.persistence.*;
 public class Member {
     @Id  // Primary Key 지정
     @GeneratedValue(strategy = GenerationType.IDENTITY)  // AUTO_INCREMENT 설정 (id값이 null일 경우 자동 생성)
-    @Column(name = "memid")  // 컬럼 지정
-    private long id;
-    @Column(name = "mempwd")
+    @Column(name = "member_no")  // 컬럼 지정
+    private long memberSeq;
+    @Column(name = "mb_id")
+    private String id;
+
+    @Column(name = "mb_pwd")
     private String password;
 
-    @Column(name = "memname")
+    @Column(name = "mb_name")
     private String name;
 
-    @Column(name = "memrank")
+    @Column(name = "mb_rank")
     private int rank;
 }
---
+++ src/main/java/com/example/restfulAPI/JPA/Post.java
@@ -0,0 +1,29 @@
+package com.example.restfulAPI.JPA;
+
+import lombok.Setter;
+import lombok.Getter;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "post")
+@Getter
+@Setter
+public class Post {
+    @Id  // Primary Key 지정
+    @GeneratedValue(strategy = GenerationType.IDENTITY)  // AUTO_INCREMENT 설정 (id값이 null일 경우 자동 생성)
+    @Column(name = "post_no")  // 컬럼 지정
+    private long postSeq;
+    @Column(name = "board_no")
+    private String boardNo;
+
+    @Column(name = "post_subject")
+    private String subject;
+
+    @Column(name = "post_content")
+    private String content;
+
+    @Column(name = "member_no")
+    private int memberNo;
+}
+
+
